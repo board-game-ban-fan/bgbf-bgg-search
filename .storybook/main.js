24f059e85815d0a34503f330ed40cf902a09a95e
GoreStarry <sunrise91.t3@gmail.com>
2022-02-24 01:34:40 +0800

    [update] v0.1

---
+++ .storybook/main.js
@@ -0,0 +1,31 @@
+module.exports = {
+  core: {
+    builder: "webpack5",
+  },
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app",
+    "@storybook/addon-knobs",
+    "@storybook/addon-storysource",
+  ],
+  // webpackFinal: async (config) => {
+  //   config.module.rules.push({
+  //     test: /\.scss$/,
+  //     use: ["style-loader", "css-loader", "sass-loader"],
+  //     include: path.resolve(__dirname, "../")
+  //   });
+
+  //   config.module.rules.push({
+  //     test: /\.(ts|tsx)$/,
+  //     loader: require.resolve("babel-loader"),
+  //     options: {
+  //       presets: [["react-app", { flow: false, typescript: true }]]
+  //     }
+  //   });
+  //   config.resolve.extensions.push(".ts", ".tsx");
+
+  //   return config;
+  // }
+};
---
+++ rollup.config.js
@@ -0,0 +1,69 @@
+/* eslint-disable import/no-anonymous-default-export */
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import postcss from "rollup-plugin-postcss";
+import image from "@rollup/plugin-image";
+import json from "@rollup/plugin-json";
+import { terser } from "rollup-plugin-terser";
+import babel from "rollup-plugin-babel";
+import del from "rollup-plugin-delete";
+import replace from "rollup-plugin-replace";
+
+const packageJson = require("./package.json");
+
+const defaultSettings = {
+  plugins: [
+    // peerDepsExternal(),
+    replace({
+      "process.env.NODE_ENV": JSON.stringify("production"),
+    }),
+    resolve(),
+    commonjs({
+      // include: /node_modules/
+    }),
+    babel({
+      exclude: "node_modules/**",
+    }),
+    // del({ targets: ["dist/*"] }),
+    postcss({
+      modules: true,
+      extract: false,
+      // writeDefinitions: true,
+      // inject: true,
+    }),
+    image(),
+    json(),
+    terser(),
+  ],
+};
+
+export default [
+  {
+    input: "src/lib.js",
+    output: [
+      {
+        file: packageJson.main,
+        format: "cjs",
+        sourcemap: true,
+      },
+      {
+        file: packageJson.module,
+        format: "esm",
+        sourcemap: true,
+      },
+    ],
+    ...defaultSettings,
+  },
+  // {
+  //   input: "src/script.js",
+  //   output: [
+  //     {
+  //       file: "dist/gltfDisplayer.js",
+  //       format: "iife", // for header script
+  //       sourcemap: true,
+  //     },
+  //   ],
+  //   ...defaultSettings,
+  // },
+];
